72128102fdda46e89287b2ab3f729672c74dfaf7
Gertjan van Heertum <gertjan.vanheertum@eurocom-group.eu>
2020-12-14 13:06:42 +0100

    initial test

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MCListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var mcastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                int mcastPort = 30011;
+                IPAddress mcastAddress = IPAddress.Parse("236.99.250.121");                
+                IPAddress localIPAddr = IPAddress.Parse("172.16.23.158");                
+                IPEndPoint localEP = new IPEndPoint(IPAddress.Any, mcastPort);          
+                mcastSocket.Bind(localEP);
+
+                MulticastOption option = new MulticastOption(mcastAddress, localIPAddr);              
+                mcastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, option);
+
+                EndPoint remoteEp = new IPEndPoint(localIPAddr, mcastPort);
+
+                byte[] arr = new byte[4096];
+                
+                while (true)
+                {
+                    var receivedBytes = mcastSocket.ReceiveFrom(arr, ref remoteEp);
+                    var str = System.Text.Encoding.ASCII.GetString(arr).Substring(0, receivedBytes);
+                    Console.WriteLine($"Got data {receivedBytes} bytes: {str}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ sendMC message.ps1
@@ -0,0 +1,52 @@
+﻿<#  
+.SYNOPSIS 
+    Sends a UDP datagram to a port 
+.DESCRIPTION 
+    This script used system.net.socckets to send a UDP 
+    datagram to a particular port. Being UDP, there's 
+    no way to determine if the UDP datagram actually 
+    was received.  
+    for this sample, a port was chosen (20000). 
+.NOTES 
+    File Name  : Send-UDPDatagram 
+    Author     : Thomas Lee - tfl@psp.co.uk 
+    Requires   : PowerShell V2 CTP3 
+.LINK 
+    http://www.pshscripts.blogspot.com 
+.EXAMPLE 
+#> 
+
+### 
+#  Start of Script 
+## 
+
+# Define port and target IP address 
+# Random here! 
+[int] $Port = 30011 
+$IP = "236.99.250.121" 
+$Address = [system.net.IPAddress]::Parse($IP) 
+
+# Create IP Endpoint 
+$End = New-Object System.Net.IPEndPoint $address, $port 
+
+# Create Socket 
+$Saddrf   = [System.Net.Sockets.AddressFamily]::InterNetwork 
+$Stype    = [System.Net.Sockets.SocketType]::Dgram 
+$Ptype    = [System.Net.Sockets.ProtocolType]::UDP 
+$Sock     = New-Object System.Net.Sockets.Socket $saddrf, $stype, $ptype 
+$Sock.TTL = 26 
+
+# Connect to socket 
+$sock.Connect($end) 
+
+# Create encoded buffer 
+$Enc     = [System.Text.Encoding]::ASCII 
+$Message = "Bliep"*10 
+$Buffer  = $Enc.GetBytes($Message) 
+
+# Send the buffer 
+$Sent   = $Sock.Send($Buffer) 
+"{0} characters sent to: {1} " -f $Sent,$IP 
+"Message is:" 
+$Message 
+# End of Script 
